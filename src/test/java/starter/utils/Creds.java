218d3ded9311b8f3dc5946aa945432345006f209
Terlitsa <terlitsa@yahoo.com>
2022-05-31 20:41:46 +0300

    Changing bad XPaths

--- src/test/java/starter/utils/Creds.java
+++ src/test/java/starter/utils/Creds.java
@@ -4,5 +4,5 @@ public class Creds {
 
     public static String USER_NAME = "dterlita";
     public static String USER_PASSWORD = "Xz232d16BIO2";
-    public static String USER_AUTH_KEY = "GBJUI3LOKM2VOUTEKZWECMDNGVCGG4ZS";
+    public static String USER_AUTH_KEY = "KRWUIQKOIRFFO23YMQ4EKVLVORCU22RR";
 }
